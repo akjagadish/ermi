83cfcafa5528690d710f6548cbdc354b7be20950
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-01-18 14:12:05 +0100

    generate tasks from LLMs

---
+++ categorisation/generate_tasks/generate_tasks.py
@@ -0,0 +1,234 @@
+import openai
+import gym
+import time
+import pandas as pd
+import numpy as np
+import torch
+import argparse
+import sys
+sys.path.append("..")
+sys.path.insert(1, '/u/ajagadish/vanilla-llama/')
+sys.path.insert(1, '/raven/u/ajagadish/vanilla-llama/')
+from inference import LLaMAInference
+from prompts import retrieve_prompt
+from utils import retrieve_features_and_categories, get_regex_patterns
+import ipdb
+import pickle
+import re
+import os
+from dotenv import load_dotenv
+import anthropic
+load_dotenv() # load environment variables from .env
+TOKEN_COUNTER = 0
+
+# generate action using LLaMA or GPT-3
+def act(text=None, run_gpt='llama', temperature=1., max_length=300):
+
+    global TOKEN_COUNTER
+
+    if run_gpt=='llama':
+
+        #TODO: use stop words or maybe try to resample whenever it gives wierd results
+        raw_response = llama.generate([text], temperature=temperature, max_length=max_length)[0][0]
+        return raw_response
+    
+    elif run_gpt=='gpt4':
+        
+        openai.api_key = os.getenv("OPENAI_API_KEY_GPT4") # load key from env
+        text = [{"role": "system", "content": "Do not generate any text other than the list of objects with their feature values and their corresponding category label in the format specified by the user."}, \
+                {"role": "user", "content": text}]
+        engine = 'gpt-4'
+        try:
+            response = openai.ChatCompletion.create(
+                model = engine,
+                messages = text,
+                max_tokens = max_length,
+                temperature = temperature,
+            )
+            TOKEN_COUNTER += response['usage']['total_tokens'] 
+            return response.choices[0].message.content#.replace(' ', '')
+        except:
+            print("Error, trying again...ratelimiterror")
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            print(exc_value)
+
+
+    elif run_gpt=='gpt3':
+        
+        openai.api_key = os.getenv("OPENAI_API_KEY") # load key from env
+        engine = "text-davinci-003"
+        try:
+            response = openai.Completion.create(
+                engine = engine,
+                prompt = text,
+                max_tokens = max_length,
+                temperature = temperature,
+            )
+            TOKEN_COUNTER += response['usage']['total_tokens'] 
+            return response.choices[0].text.strip()#.replace(' ', '')
+        except:
+            print("Error")
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            print(exc_value)
+            #time.sleep(3**iter)
+            
+    elif run_gpt=='claude':
+        
+        try:
+            client = anthropic.Anthropic()
+            response = client.completions.create(
+                    prompt = anthropic.HUMAN_PROMPT + text + anthropic.AI_PROMPT,
+                    #stop_sequences=[anthropic.HUMAN_PROMPT],
+                    model="claude-2",
+                    temperature=temperature,
+                    max_tokens_to_sample=max_length,
+                ).completion#.replace(' ', '')
+
+        except:
+            print("Error")
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            print(exc_value)
+            # time.sleep(3**iter)
+        return response
+
+    elif run_gpt=='claude_2.1':
+
+        client = anthropic.Anthropic()
+        response = client.completions.create(
+                prompt = anthropic.HUMAN_PROMPT + text + anthropic.AI_PROMPT,
+                #stop_sequences=[anthropic.HUMAN_PROMPT],
+                model="claude-2.1",
+                temperature=temperature,
+                max_tokens_to_sample=max_length,
+            ).completion#.replace(' ', '')
+
+        return response
+ 
+    else:
+
+        return NotImplementedError 
+
+# check if action is parsable and if yes, return the matches
+def check_if_parsable(action, patterns):
+    for pattern in patterns:
+        matches = re.findall(pattern, action.replace(' ', ''), re.MULTILINE)
+        if len(matches) >0:
+            return matches
+    return None
+
+if __name__ == "__main__":
+    models = ["7B", "13B", "30B", "65B", "NA"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--llama-path", type=str, required=False, default=None)
+    parser.add_argument("--model", type=str, required=False, choices=models)
+    parser.add_argument("--run-gpt", type=str, required=True, choices=['llama', 'gpt3', 'gpt4', 'claude', 'claude_2.1'])
+    parser.add_argument("--num-tasks", type=int, required=True, default=1000)
+    parser.add_argument("--num-dim", type=int, required=True, default=3)
+    parser.add_argument("--num-data", type=int, required=True, default=8)
+    parser.add_argument("--temperature", type=float, required=False, default=1.0)
+    parser.add_argument("--max-length", type=int, required=False, default=300)
+    parser.add_argument("--proc-id", type=int, required=False, default=0)
+    parser.add_argument("--num-runs", type=int, required=False, default=1)
+    parser.add_argument("--prompt-version", type=str, required=False, default=None)
+    parser.add_argument("--use-generated-tasklabels", action="store_true", required=False, default=False)
+    parser.add_argument("--path-tasklabels", type=str, required=False, default='/raven/u/ajagadish/vanilla-llama/categorisation/data/tasklabels')
+    parser.add_argument("--file-name-tasklabels", type=str, required=False, default=None)
+    parser.add_argument("--start-task-id", type=int, required=False, default=0)
+    parser.add_argument("--end-task-id", type=int, required=False, default=None)
+    parser.add_argument('--stage', type=int, default=0, help='stage of prompt generation')
+
+    args = parser.parse_args()
+    start_loading = time.time()
+    run_gpt = args.run_gpt 
+    assert args.model=='NA'if args.run_gpt=='gpt3' or args.run_gpt=='gpt4' or args.run_gpt=='claude' or args.run_gpt=='claude_2.1' else False, "Only NA model is supported for GPT3"
+    # model parameters
+    temperature = args.temperature
+    max_length = args.max_length
+    # instruction parameters
+    start_task_id = args.start_task_id
+    num_tasks = args.num_tasks
+    num_data = args.num_data
+    num_dim = args.num_dim
+    # runtime parameters
+    proc_id = args.proc_id
+    num_runs = args.num_runs
+    prompt_version = args.prompt_version
+    num_categories = 2
+
+
+    # get regex patterns
+    patterns = get_regex_patterns(num_dim=num_dim, use_generated_tasklabels=args.use_generated_tasklabels, prompt_version=int(prompt_version))
+
+    # load LLaMA model and instructions
+    if run_gpt == 'llama':
+        llama = LLaMAInference(args.llama_path, args.model, max_batch_size=2)
+        instructions = retrieve_prompt('llama', version='v0', num_dim=num_dim, num_data=num_data)
+
+    # load GPT-3 specific instructions
+    elif run_gpt == 'gpt3':
+        instructions = retrieve_prompt('gpt3', version='v1', num_dim=num_dim, num_data=num_data)
+
+    # load GPT-4 specific instructions
+    elif run_gpt == 'gpt4':
+        instructions = retrieve_prompt('gpt4', version='v3', num_dim=num_dim, num_data=num_data)
+    
+    # load Claude specific instructions
+    # elif run_gpt == 'claude':
+    #     instructions = retrieve_prompt('claude', version=f'v{prompt_version}', num_dim=num_dim, num_data=num_data)
+
+    # run gpt models
+    for run in range(num_runs):
+        data, unparsable_data, raw_data, task_ids = [], [], [], []
+        end_task_id = start_task_id+num_tasks if args.end_task_id is None else args.end_task_id
+        for idx, t in enumerate(range(start_task_id, end_task_id)):
+
+            ## LLM acts
+            if (run_gpt == 'claude'or run_gpt == 'claude_2.1') and args.use_generated_tasklabels:
+                assert args.file_name_tasklabels is not None, "Please provide a file name for the task labels"
+                
+                features, categories, task_id = retrieve_features_and_categories(path=args.path_tasklabels,\
+                                                                        file_name=args.file_name_tasklabels,\
+                                                                        task_id=t)
+                assert len(features) == num_dim, "Number of features does not match the number of dimensions"
+                assert len(categories) == num_categories, "Number of categories does not match the number of categories"
+                instructions = retrieve_prompt('claude', version=f'v{prompt_version}', num_dim=num_dim, num_data=num_data, features=features, categories=categories)
+           
+           ## generate tasks in one or two stages
+            if args.stage == 2:
+                with open(f"data/raw_data/{run_gpt}_generated_tasks_params{args.model}_dim{num_dim}_data{num_data}_tasks{num_tasks}_run{run}_procid{proc_id}_pversion{prompt_version}_stage1_starttaskid{start_task_id}_raw.txt", "rb") as fp:   
+                    stage1_action = pickle.load(fp)[idx]
+                matches = check_if_parsable(stage1_action, patterns)
+                if matches is not None: # the original few points are getting changed
+                    stage2_action = act(instructions + stage1_action[:int(len(stage1_action)/3)], run_gpt, temperature, max_length)
+                    action = stage2_action + '\n' + stage1_action[int(len(stage1_action)/3):] # action already contains the first part of the action
+            else:
+                action = act(instructions, run_gpt, temperature, max_length)
+            
+            raw_data.append(action)
+            matches = check_if_parsable(action, patterns)
+            if matches is not None:
+                data.append(matches)
+                task_ids.append(task_id)
+            elif matches is None:
+                unparsable_data.append(action)
+
+            
+            print(f'task {t}: no matches found' if matches is None else f'task {t}: match found')
+            
+            # save data using pickle
+            filename = f'{run_gpt}_generated_tasks_params{args.model}_dim{num_dim}_data{num_data}_tasks{num_tasks}_run{run}_procid{proc_id}_pversion{prompt_version}'
+            filename += f"_stage{str(args.stage)}" if args.stage > 0 else ""
+            with open(f"data/parsed/{filename}.txt", "wb") as fp:   
+                pickle.dump(data, fp)
+
+            with open(f"data/parsed/{filename}_taskids.txt", "wb") as fp:
+                pickle.dump(task_ids, fp)
+
+            with open(f"data/unparsed/{filename}_unparsed.txt", "wb") as fp:   
+                pickle.dump(unparsable_data, fp)
+
+            with open(f"data/raw_data/{filename}_starttaskid{start_task_id}_raw.txt", "wb") as fp:
+                pickle.dump(raw_data, fp)
+
+    if run_gpt == 'gpt4':
+        print(f'total tokens used: {TOKEN_COUNTER}')
---
+++ categorisation/generate_tasks/utils.py
@@ -0,0 +1,104 @@
+import pickle
+import re
+import pandas as pd
+import numpy as np
+import torch
+import sys
+SYS_PATH = '/u/ajagadish/vanilla-llama/categorisation'
+sys.path.append(f"{SYS_PATH}/categorisation/rl2")
+sys.path.append(f"{SYS_PATH}/categorisation/data")
+sys.path.append(f"{SYS_PATH}/categorisation/")
+
+def return_generated_task(path, gpt, model, num_dim, num_data, num_tasks, run, proc_id, prompt_version, stage):
+    filename = f'{gpt}_generated_tasks_params{model}_dim{num_dim}_data{num_data}_tasks{num_tasks}_run{run}_procid{proc_id}_pversion{prompt_version}'
+    if stage>=1:
+        filename = f'{filename}_stage{stage}'
+    return pd.read_csv(f"{path}/{filename}.csv")
+
+def find_counts(inputs, dim, xx_min, xx_max):
+    return (inputs[:, dim]<xx_max)*(inputs[:, dim]>xx_min)
+
+def pool_tasklabels(path_to_dir, run_gpt, model, num_dim, num_tasks, num_runs, proc_id, prompt_version, num_categories=2):
+    df, last_task_id = None, 0
+    for run_id in range(num_runs):
+        data = None
+        try:
+            filename = f'{run_gpt}_generated_tasklabels_params{model}_dim{num_dim}_tasks{num_tasks}_run{run_id}_procid{proc_id}_pversion{prompt_version}'
+            data = pd.read_csv(f'{path_to_dir}/{filename}.csv')       
+        except:
+            print(f'error loading {filename}')
+        if data is not None:
+            # does number of features match the number of dimensions
+            features = [eval(feature) for feature in data.feature_names.values]
+            features_match = np.array([len(feature) for feature in features])==num_dim 
+            # does number of categories match the number of dimensions
+            categories = [eval(category) for category in data.category_names.values]
+            categories_match = np.array([len(category) for category in categories])==num_categories
+            # if both match, add to dataframe
+            both_match = features_match*categories_match
+            processed_data = pd.DataFrame({'feature_names': data.feature_names.values[both_match], 'category_names': data.category_names.values[both_match], 'task_id': np.arange(len(data.task_id.values[both_match])) + last_task_id})
+            df = processed_data if df is None else pd.concat([df, processed_data], ignore_index=True)
+            last_task_id = df.task_id.values[-1] + 1
+
+
+    num_tasks = df.task_id.max()+1
+    # df.feature_names = df['feature_names'].apply(lambda x: eval(x))
+    # df.category_names = df['category_names'].apply(lambda x: eval(x))
+    df.to_csv(f'{path_to_dir}/{run_gpt}_generated_tasklabels_params{model}_dim{num_dim}_tasks{num_tasks}_pversion{prompt_version}.csv')             
+
+def get_regex_patterns(num_dim, use_generated_tasklabels, prompt_version):
+    ''' 
+    Generate regex patterns to parse the generated tasks
+    Args:
+        num_dim: number of dimensions
+        use_generated_tasklabels: whether to use the generated tasklabels or not
+        prompt_version: version of the prompt used to generate the tasks
+    Returns:
+        patterns: list of regex patterns
+    '''
+    if use_generated_tasklabels is False:
+        og_regex_expressions = [r'x=\[(.*?)\][;,]?\s*y\s*=?\s*([AB])',
+                        r"x=\[(.*?)\][^\n]*?y=(\w)",
+                        r"x=\[([\d\.]+),\s*([\d\.]+),\s*([\d\.]+)\][^\n]*[y|&]=\s*(A|B)",
+                        r"x=\[?\s*([\d\.]+),\s*([\d\.]+),\s*([\d\.]+)\]?\s*(?:,|;|&|->| -> |---)?\s*[y|Y]\s*=\s*(A|B)",
+                        r"x=(\[.*?\])\s*---\s*y\s*=\s*([A-Z])",
+                        r"x=(\[.*?\])\s*->\s*([A-Z])",
+                        r"x=(\[.*?\]),\s*([A-Z])",
+                        r"^([0-9]\.[0-9]{2}),(0\.[0-9]{2}),(0\.[0-9]{2}),(A|B)$",
+                        r"\[([0-9]\.[0-9]{2}),(0\.[0-9]{2}),(0\.[0-9]{2})\],(A|B)",
+                        r"\[\[([0-9]\.[0-9]{2}),(0\.[0-9]{2}),(0\.[0-9]{2})\],(A|B)\]",
+                        r"n[0-9]+\.\[\[([0-9]\.[0-9]{2}),(0\.[0-9]{2}),(0\.[0-9]{2})\],(\'A\'|\'B\')\]",
+                        r"\[\[([0-9]\.[0-9]{2}),(0\.[0-9]{2}),(0\.[0-9]{2})\],(\'A\'|\'B\')\]",
+                        r"\[([0-9]\.[0-9]{2}),(0\.[0-9]{2}),(0\.[0-9]{2})\],(A|B)",
+                        r"(\d+\.\d+),(\d+\.\d+),(\d+\.\d+),([A-Z])"
+                        ] 
+    elif num_dim == 3 and prompt_version == 4: 
+        regex_expressions = [r'([\d.]+),([\d.]+),([\d.]+),([\w]+)',
+                r'([\w\-]+),([\w\-]+),([\w\-]+),([\w]+)',
+                r'([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+)',
+                r'([^,]+),([^,]+),([^,]+),([^,]+)',
+                r'([^,\n]+),([^,\n]+),([^,\n]+),([^,\n]+)',
+                r'(?:.*?:)?([^,-]+),([^,-]+),([^,-]+),([^,-]+)',
+                r'([^,-]+),([^,-]+),([^,-]+),([^,-]+)',]
+                        
+    elif num_dim == 6 and prompt_version == 5: 
+        regex_expressions = [r'^(\d+):([\d.]+),([\d.]+),([\d.]+),([\d.]+),([\d.]+),([\d.]+),([\w]+)',
+                r'^(\d+):([\w\-]+),([\w\-]+),([\w\-]+),([\w\-]+),([\w\-]+),([\w\-]+),([\w]+)',
+                r'^(\d+):([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+)',
+                r'^(\d+):([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+)',
+                r'^(\d+):([^,\n]+),([^,\n]+),([^,\n]+),([^,\n]+),([^,\n]+),([^,\n]+),([^,\n]+)',
+                r'^(\d+):(?:.*?:)?([^,-]+),([^,-]+),([^,-]+),([^,-]+),([^,-]+),([^,-]+),([^,-]+)',
+                r'^(\d+):([^,-]+),([^,-]+),([^,-]+),([^,-]+),([^,-]+),([^,-]+),([^,-]+)',]
+
+    elif num_dim == 4 and prompt_version == 5:
+        regex_expressions = [r'^(\d+):([\d.]+),([\d.]+),([\d.]+),([\d.]+),([\w]+)',
+                r'^(\d+):([\w\-]+),([\w\-]+),([\w\-]+),([\w\-]+),([\w]+)',
+                r'^(\d+):([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+),([-\w\d,.]+)',
+                r'^(\d+):([^,]+),([^,]+),([^,]+),([^,]+),([^,]+)',
+                r'^(\d+):([^,\n]+),([^,\n]+),([^,\n]+),([^,\n]+),([^,\n]+)',
+                r'^(\d+):(?:.*?:)?([^,-]+),([^,-]+),([^,-]+),([^,-]+),([^,-]+)',
+                r'^(\d+):([^,-]+),([^,-]+),([^,-]+),([^,-]+),([^,-]+)']
+                         
+    patterns = regex_expressions if use_generated_tasklabels else og_regex_expressions
+
+    return patterns           
