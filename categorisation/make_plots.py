aae34d100b3f849b700ef87b2a8cfdb8f8c4a433
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-04-02 13:31:15 +0200

    replicated all figures submitted to ICML

--- categorisation/make_plots.py
+++ categorisation/make_plots.py
@@ -1,10 +1,27 @@
-from plots import model_comparison_badham2017, model_simulations_smith1998, model_simulations_shepard1961, model_comparison_devraj2022, model_comparison_johanssen2002, plot_dataset_statistics
+from plots import model_comparison_badham2017, model_simulations_smith1998, model_simulations_shepard1961, model_comparison_devraj2022, model_comparison_johanssen2002, plot_dataset_statistics, compare_data_statistics, compare_inputfeatures
 
-plot_dataset_statistics(0)
-plot_dataset_statistics(1)
-plot_dataset_statistics(2)
-model_simulations_shepard1961()
-model_comparison_badham2017()
-model_simulations_smith1998()
-model_comparison_devraj2022()
-model_comparison_johanssen2002()
+# plot_dataset_statistics(0)
+# plot_dataset_statistics(1)
+# plot_dataset_statistics(2)
+# plot_dataset_statistics(3)
+# model_simulations_shepard1961('main')
+# model_simulations_shepard1961('supplementary')
+# model_comparison_badham2017()
+# model_simulations_smith1998('main')
+# model_simulations_smith1998('supplementary')
+# model_comparison_devraj2022()
+# model_comparison_johanssen2002('main')
+model_comparison_johanssen2002('supplementary')
+
+
+## rebuttals
+# model_simulations_shepard1961(num_blocks=6)
+# model_simulations_shepard1961('supplementary', num_blocks=6)
+# compare_data_statistics([2,0])
+# compare_data_statistics([2,1])
+# compare_data_statistics([2,3])
+# model_simulations_shepard1961('rebuttals', num_blocks=6)
+# compare_inputfeatures([2, 0,])
+# compare_inputfeatures([2, 0, 1, 3])
+
+
