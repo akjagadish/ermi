a69fc8155bb3bf1bdeeee1a34d09cee82e479958
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-04-25 08:39:00 +0200

    update scripts

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -19,7 +19,7 @@ module load anaconda/3/2021.11
 module load gcc/11 impi/2021.6
 module load cuda/11.6
 module load pytorch_distributed/gpu-cuda-11.6/1.13.0
-pip3 install --user accelerate openai gym ipdb transformers tensorboard anthropic openml
+pip3 install --user accelerate openai gym ipdb transformers tensorboard anthropic openml wordcloud mycolorpy Pillow
 clear
 jupyter-lab
 
---
+++ scripts/run_metalearner_simulation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+#SBATCH -o ./logs/%A.out
+#SBATCH -e ./logs/%A.err
+#SBATCH --job-name=simulate_metalearner
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=akshaykjagadish@gmail.com
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=18
+
+
+cd ~/vanilla-llama/categorisation/
+
+module purge
+module load anaconda/3/2023.03
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+
+## johannsen task
+python rl2/simulate.py --job-id ${SLURM_ARRAY_TASK_ID} --experiment johanssen_categorisation --num-runs 1 --model-name env=claude_generated_tasks_paramsNA_dim4_data650_tasks8950_pversion5_stage1_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0
+# python rl2/simulate.py --job-id ${SLURM_ARRAY_TASK_ID} --experiment johanssen_categorisation --num-runs 1 --model-name env=dim4synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1_synthetic 
+#python rl2/simulate.py --job-id ${SLURM_ARRAY_TASK_ID} --experiment johanssen_categorisation --num-runs 1 --model-name env=dim4synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1_syntheticnonlinear
+
+## smith task
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.  --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.1 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.2 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.3 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.4 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.5 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.6 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.7 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.8 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.9 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 1.0 --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+
+## shepard task
+# python rl2/simulate.py --experiment shepard_categorisation --model-name env=claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
