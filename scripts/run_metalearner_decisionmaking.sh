63bd4bb826c5b1f6554f58edeeb7d5cb0014d4e8
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-05-08 18:21:23 +0200

    update scripts

--- scripts/run_metalearner_decisionmaking.sh
+++ scripts/run_metalearner_decisionmaking.sh
@@ -15,4 +15,8 @@ module load cuda/11.6
 module load anaconda/3/2021.11 # need to use old anaconda version so it uses torch that is compatible with cuda 11.6 and allows using GPU
 pip3 install --user ipdb transformers tensorboard ipdb tqdm 
 
-python mi/train_decisionmaking.py --num-episodes 100000 --max-steps 20 --num-dims 2 --loss bce --env-name claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2 --env-dir /u/ajagadish/ermi/decisionmaking/data --save-dir /u/ajagadish/ermi/decisionmaking/trained_models/ --save-every 100 --print-every 10  --first-run-id 0 --noise 0.0 --model-name transformer --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8 --batch_size 64 --shuffle 
+# per feature minmax normalization
+# python mi/train_decisionmaking.py --num-episodes 100000 --max-steps 20 --num-dims 2  --first-run-id 0 --loss bce --env-name claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2 --env-dir /u/ajagadish/ermi/decisionmaking/data --save-dir /u/ajagadish/ermi/decisionmaking/trained_models/ --save-every 100 --print-every 10  --noise 0.0 --model-name transformer --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8 --batch_size 64 --shuffle 
+
+# max steps is set to 10
+python mi/train_decisionmaking.py --synthetic --env-name dim2synthetic --num-episodes 100000 --max-steps 10 --num-dims 2  --first-run-id 0 --loss bce --save-dir /u/ajagadish/ermi/decisionmaking/trained_models/ --save-every 100 --print-every 10  --noise 0.0 --model-name transformer --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8 --batch_size 64 --shuffle 
---
+++ scripts/run_modelfitting.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -l
+#SBATCH -o ./logs/%A.out
+#SBATCH -e ./logs/%A.err
+#SBATCH --job-name=fit_humansHumans
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=akshaykjagadish@gmail.com
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=18
+
+
+cd ~/ermi/
+module purge
+module load cuda/11.6
+module load anaconda/3/2023.03
+pip3 install --user accelerate openai gym ipdb transformers tensorboard anthropic openml wordcloud mycolorpy Pillow pyro-ppl 
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+
+# optimize model parameters for meta-learners
+
+##  binz2022
+python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --model-name env=claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2_model=transformer_num_episodes100000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0
+
+## badham2017
+# python model_comparison/fit_humans.py --model-name env=claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=3 --task-name badham2017 --optimizer
+# python model_comparison/fit_humans.py --model-name env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic  --task-name badham2017 --optimizer
+# python model_comparison/fit_humans.py --model-name env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_syntheticnonlinear  --task-name badham2017 --optimizer
+# python model_comparison/fit_humans.py --model-name env=rmc_tasks_dim3_data100_tasks11499_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1_rmc  --task-name badham2017 --optimizer
+
+
+## devraj2022
+# python model_comparison/fit_humans.py --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1 --task-name devraj2022 --optimizer
+# python model_comparison/fit_humans.py --model-name env=dim6synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=2_synthetic  --task-name devraj2022 --optimizer
+# python model_comparison/fit_humans.py --model-name env=dim6synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=2_syntheticnonlinear  --task-name devraj2022 --optimizer
+# python model_comparison/fit_humans.py --model-name env=rmc_tasks_dim6_data600_tasks12911_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_rmc  --task-name devraj2022 --optimizer
+
+
+# baseline models
+
+## badham2017
+# python baselines/run_gcm.py --num-iter 1 --loss 'nll' --num-blocks 1 --fit-human-data --task-name badham2017
+# python baselines/run_pm.py --num-iter 1 --loss 'nll' --num-blocks 1 --fit-human-data --prototypes from_data --task-name badham2017
+
+## devraj2022
+# python baselines/run_gcm.py --num-iter 1 --loss 'nll' --num-blocks 1 --fit-human-data --task-name devraj2022
+# python baselines/run_pm.py --num-iter 1 --loss 'nll' --num-blocks 1 --fit-human-data --prototypes from_data --task-name devraj2022
+
+
+# grid search for meta-learners
+
+### beta sweep
+
+## badham2017
+# python model_comparison/fit_humans.py --model-name env=claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0 --task-name badham2017
+# python model_comparison/fit_humans.py --model-name env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic  --task-name badham2017
+# python model_comparison/fit_humans.py --model-name env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_syntheticnonlinear  --task-name badham2017
+
+## devraj2022
+# python model_comparison/fit_humans.py --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1 --task-name devraj2022
+# python model_comparison/fit_humans.py --model-name env=dim6synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic  --task-name devraj2022
+# python model_comparison/fit_humans.py --model-name env=dim6synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_syntheticnonlinear  --task-name devraj2022
+
+### epsilon sweep
+
+# badham2017
+# python model_comparison/fit_humans.py --model-name env=claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0 --task-name badham2017 --method eps_greedy
+# python model_comparison/fit_humans.py --model-name env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic  --task-name badham2017 --method eps_greedy
+# python model_comparison/fit_humans.py --model-name env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_syntheticnonlinear  --task-name badham2017 --method eps_greedy
+
+# devraj2022
+# python model_comparison/fit_humans.py --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1 --task-name devraj2022 --method eps_greedy
+# python model_comparison/fit_humans.py --model-name env=dim6synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic  --task-name devraj2022 --method eps_greedy
+# python model_comparison/fit_humans.py --model-name env=dim6synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_syntheticnonlinear  --task-name devraj2022 --method eps_greedy
+
