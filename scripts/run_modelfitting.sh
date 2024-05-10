b1824209c8b6b01f8b4cbd80b6926ed0e395556e
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-05-10 16:33:50 +0200

    update scripts

--- scripts/run_modelfitting.sh
+++ scripts/run_modelfitting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -o ./logs/%A.out
 #SBATCH -e ./logs/%A.err
-#SBATCH --job-name=fit_humansHumans
+#SBATCH --job-name=fit_humans
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akshaykjagadish@gmail.com
 #SBATCH --time=24:00:00
@@ -19,7 +19,9 @@ export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 # optimize model parameters for meta-learners
 
 ##  binz2022
-python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --model-name env=claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2_model=transformer_num_episodes100000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0
+python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --num-iters 1 --model-name env=claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0
+# python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --num-iters 1 --model-name env=synthetic_model=transformer_num_episodes100000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic
+python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --num-iters 1 --model-name env=claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2_model=transformer_num_episodes100000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0
 
 ## badham2017
 # python model_comparison/fit_humans.py --model-name env=claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=3 --task-name badham2017 --optimizer
