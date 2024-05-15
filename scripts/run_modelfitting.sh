34cffc938676393c5f7011422630bfca75856c2d
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-05-15 10:57:26 +0200

    update model iftting scripts

--- scripts/run_modelfitting.sh
+++ scripts/run_modelfitting.sh
@@ -17,22 +17,17 @@ export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
 
 # optimize model parameters for meta-learners
-
 ##  binz2022
-python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --num-iters 1 --model-name env=claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0
-# python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --num-iters 1 --model-name env=synthetic_model=transformer_num_episodes100000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic
-# python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --num-iters 1 --model-name env=claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2_model=transformer_num_episodes100000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0
-
-# paired model on binz2022
-# python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --paired --num-iters 1 --model-name env=claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2_unknown_model=transformer_num_episodes100000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_pairedTrue_run=0
-# python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --paired --num-iters 1 --model-name env=synthetic_model=transformer_num_episodes100000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_pairedTrue_run=0_synthetic
+python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --num-iters 1 --model-name env=claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2_unknown_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_pairedFalse_run=0
+python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --num-iters 1 --model-name env=synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic
+python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --paired --num-iters 1 --model-name env=claude_generated_functionlearningtasks_paramsNA_dim2_data20_tasks9254_run0_procid0_pversion2_unknown_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_pairedTrue_run=0
+python mi/fit_humans.py --paradigm decisionmaking --task-name binz2022 --optimizer --paired --num-iters 1 --model-name env=synthetic_model=transformer_num_episodes100000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_pairedTrue_run=0_synthetic
 
 ## badham2017
 # python model_comparison/fit_humans.py --model-name env=claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=3 --task-name badham2017 --optimizer
 # python model_comparison/fit_humans.py --model-name env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic  --task-name badham2017 --optimizer
 # python model_comparison/fit_humans.py --model-name env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_syntheticnonlinear  --task-name badham2017 --optimizer
 # python model_comparison/fit_humans.py --model-name env=rmc_tasks_dim3_data100_tasks11499_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1_rmc  --task-name badham2017 --optimizer
-
 
 ## devraj2022
 # python model_comparison/fit_humans.py --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1 --task-name devraj2022 --optimizer
