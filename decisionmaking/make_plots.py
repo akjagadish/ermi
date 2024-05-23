af34111ced6f065cf2530bac6f02de0f4a11dba6
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-05-23 10:56:32 +0200

    code to save and plot regression data stats

--- decisionmaking/make_plots.py
+++ decisionmaking/make_plots.py
@@ -1,19 +1,30 @@
 # , compare_data_statistics, compare_inputfeatures, plot_frequency_tasklabels, compare_stats_across_models
 from plots import plot_decisionmaking_data_statistics
-from utils import save_real_data
+from utils import save_real_data_openML, save_real_data_lichtenberg2017
 
 
-# save_real_data(k=4, num_points=20)
-# plot_decisionmaking_data_statistics(0, dim=2, condition='unknown')
-# plot_decisionmaking_data_statistics(2, dim=2, condition='real')
+# extract real world data
+save_real_data_openML(k=4, num_points=20)
+save_real_data_lichtenberg2017(k=4, num_points=20)
+save_real_data_lichtenberg2017(k=4, num_points=20, method='random')
+save_real_data_openML(k=4, num_points=20, method='random')
+
+# plot data statistics for 2D data
+plot_decisionmaking_data_statistics(0, dim=2, condition='unknown')
+plot_decisionmaking_data_statistics(2, dim=2, condition='real')
 plot_decisionmaking_data_statistics(1, dim=2, condition='synthetic')
 
-# plot_decisionmaking_data_statistics(0, dim=4, condition='ranked')
-# plot_decisionmaking_data_statistics(0, dim=4, condition='direction')
-# plot_decisionmaking_data_statistics(2, dim=4, condition='real')
+# plot data statistics for 4D data
+plot_decisionmaking_data_statistics(0, dim=4, condition='ranked')
+plot_decisionmaking_data_statistics(0, dim=4, condition='direction')
+plot_decisionmaking_data_statistics(0, dim=4, condition='unknown')
+plot_decisionmaking_data_statistics(
+    2, dim=4, condition='openML', method='random')
+plot_decisionmaking_data_statistics(
+    2, dim=4, condition='lichtenberg2017', method='random')
 
 # plot_frequency_tasklabels(
-#     'claude_generated_tasklabels_paramsNA_dim3_tasks23421_pversion5', feature_names=False, pairs=False)
+#    'claude_generated_tasklabels_paramsNA_dim3_tasks23421_pversion5', feature_names = False, pairs = False)
 # plot_frequency_tasklabels(
 #     'claude_generated_tasklabels_paramsNA_dim3_tasks23421_pversion5', feature_names=True, pairs=False)
 # plot_frequency_tasklabels(
