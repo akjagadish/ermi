ab40fd1ac3956286030684b5f37e0b781776443a
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-05-23 10:52:04 +0200

    save real world  regression datas from Lichtenberg 2018 paper

--- decisionmaking/utils.py
+++ decisionmaking/utils.py
@@ -3,9 +3,10 @@ from sklearn import preprocessing
 from sklearn.feature_selection import SelectKBest, f_regression
 import numpy as np
 import pandas as pd
+import torch
 
 
-def save_real_data(k=2, num_points=650):
+def save_real_data_openML(k=2, method='best', num_points=650):
     # 'OpenML-CTR23 - A curated tabular regression benchmarking suite'
     benchmark_suite = openml.study.get_suite(353)
     data = []
@@ -29,7 +30,7 @@ def save_real_data(k=2, num_points=650):
 
             # Select the best feature
             features = SelectKBest(
-                f_regression, k=k).fit_transform(features, targets)
+                f_regression, k=k).fit_transform(features, targets) if method == 'best' else features[:, np.random.choice(features.shape[1], k, replace=False)]
 
             if features.shape[0] < num_points:
                 xs = [features]
@@ -44,7 +45,49 @@ def save_real_data(k=2, num_points=650):
 
             df = pd.DataFrame(
                 data, columns=['input', 'target', 'trial_id', 'task_id'])
-            df.to_csv('data/real_data.csv')
+            df.to_csv(f'data/real_data_dim{k}_method{method}_openML.csv')
+
+        else:
+            print('not valid data')
+
+
+def save_real_data_lichtenberg2017(k=2, method='best', num_points=650):
+
+    data = []
+    t = 0
+    datasets = torch.load(
+        '/u/ajagadish/ermi/decisionmaking/data/dataset_torch.pth')
+    for (features, targets) in datasets:  # iterate over all tasks
+
+        if (features.shape[1] < 99999) and (features.shape[1] >= k) and (not np.isnan(features).any()):
+
+            # Normalize the features and targets
+            scaler = preprocessing.MinMaxScaler(
+                feature_range=(0, 1)).fit(features)
+            features = scaler.transform(features)
+            scaler_targets = preprocessing.MinMaxScaler(
+                feature_range=(0, 1)).fit(targets.reshape(-1, 1))
+            targets = scaler_targets.transform(targets.reshape(-1, 1))
+
+            # Select the best feature
+            features = SelectKBest(
+                f_regression, k=k).fit_transform(features, targets) if method == 'best' else features[:, np.random.choice(features.shape[1], k, replace=False)]
+
+            if features.shape[0] < num_points:
+                xs = [features]
+                ys = [targets]
+            else:
+                xs = np.array_split(features, features.shape[0] // num_points)
+                ys = np.array_split(targets, targets.shape[0] // num_points)
+            for (x, y) in zip(xs, ys):
+                for i in range(x.shape[0]):
+                    data.append([x[i].tolist(), y[i], i, t])
+                t += 1
+
+            df = pd.DataFrame(
+                data, columns=['input', 'target', 'trial_id', 'task_id'])
+            df.to_csv(
+                f'data/real_data_dim{k}_method{method}_lichtenberg2017.csv')
 
         else:
             print('not valid data')
