aab9d6b5922029f4c250c8d5556b599f0c2c4b78
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-05-27 16:44:25 +0200

    chore: Add requirements.txt and setup.py files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ermi',
+    version='0.0',
+    author='Akshay Kumar Jagadish',
+    author_email='akshaykjagadish@gmail.com',
+    description='Ecological Rational Metalearned Inference',
+    url='https://github.com/akjagadish/compositional-metarl',
+    packages=find_packages(),
+)
