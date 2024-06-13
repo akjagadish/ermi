58ac459ffff4863bfb6b3ce214e4d69549f2a42e
akjagadish <akshaykjagadish@gmail.com>
2024-06-13 17:57:58 +0200

    generate 2 dim cateogry lerning tasks using lama

--- scripts/generate_tasks_llama.sh
+++ scripts/generate_tasks_llama.sh
@@ -12,6 +12,14 @@
 
 cd ~/ermi/taskgeneration
 
+module purge
+module load anaconda/3/2023.03
+module load gcc/13 impi/2021.9
+module load cuda/12.1
+module load pytorch/gpu-cuda-12.1/2.2.0
+pip3 install --user accelerate openai gym ipdb transformers tensorboard anthropic python-dotenv
+pip3 install --upgrade transformers jinja2 torch accelerate
+
 ## categorisation
 #python generate_data.py --model NA --task categorisation --proc-id 0 --num-tasks 1000 --start-task-id 0 --num-dim 3 --num-data 100 --max-length 8000 --run-gpt llama-3 --prompt-version 5 --file-name-tasklabels claude_generated_tasklabels_paramsNA_dim3_tasks23421_pversion5 --path-tasklabels /u/ajagadish/ermi/categorisation/data/task_labels
 #python generate_data.py --model NA --task categorisation --proc-id 0 --num-tasks 1000 --start-task-id 0 --num-dim 6 --num-data 100 --max-length 8000 --run-gpt llama-3 --prompt-version 5 --file-name-tasklabels claude_generated_tasklabels_paramsNA_dim6_tasks13693_pversion5 --path-tasklabels /u/ajagadish/ermi/categorisation/data/task_labels
@@ -19,7 +27,7 @@ cd ~/ermi/taskgeneration
 #python generate_data.py --model NA --task categorisation --proc-id 0 --num-tasks 1000 --start-task-id 0 --num-dim 2 --num-data 100 --max-length 8000 --run-gpt llama-3 --prompt-version 5 --file-name-tasklabels claude_generated_tasklabels_paramsNA_dim3_tasks23421_pversion5 --path-tasklabels /u/ajagadish/ermi/categorisation/data/task_labels
 # python generate_data.py --model NA --task categorisation --proc-id 1 --num-tasks 1000 --start-task-id 373 --num-dim 3 --num-data 100 --max-length 8000 --run-gpt llama-3 --prompt-version 5 --file-name-tasklabels claude_generated_tasklabels_paramsNA_dim3_tasks23421_pversion5 --path-tasklabels /u/ajagadish/ermi/categorisation/data/task_labels
 #python generate_data.py --model NA --task categorisation --proc-id 2 --num-tasks 1000 --start-task-id 5000 --num-dim 3 --num-data 100 --max-length 8000 --run-gpt llama-3 --prompt-version 5 --file-name-tasklabels claude_generated_tasklabels_paramsNA_dim3_tasks23421_pversion5 --path-tasklabels /u/ajagadish/ermi/categorisation/data/task_labels
-python generate_data.py --model NA --task categorisation --proc-id 0 --num-tasks 1000 --start-task-id 0 --num-dim 2 --num-data 100 --max-length 8000 --run-gpt llama-3 --prompt-version 5 --file-name-tasklabels claude_generated_tasklabels_paramsNA_dim3_tasks23421_pversion5 --path-tasklabels /u/ajagadish/ermi/categorisation/data/task_labels
+python generate_data.py --model NA --task categorisation --proc-id 0 --num-tasks 996 --start-task-id 0 --num-dim 2 --num-data 100 --max-length 8000 --run-gpt llama-3 --prompt-version 5 --file-name-tasklabels llama-3_synthesized_categorisation_problems_params80B_dim2_tasks996_pversion0 --path-tasklabels /u/ajagadish/ermi/categorisation/data/synthesize_problems
 
 ## function learning (proc-id 1 refers to adjusting the sorting order line of the prompt)
 # python generate_data.py --model NA --task functionlearning --proc-id 1  --num-tasks 8770 --start-task-id 0 --num-dim 4 --num-data 20 --max-length 100000 --run-gpt llama-3 --prompt-version ranked --file-name-tasklabels claude_synthesized_functionlearning_problems_paramsNA_dim4_tasks8770_pversionranked  --path-tasklabels /u/ajagadish/ermi/decisionmaking/data/synthesize_problems
