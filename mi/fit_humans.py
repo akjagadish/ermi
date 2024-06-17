b07afcb7cb19bf2eaa66dd6fb0abd083f74b4417
akjagadish <akshaykjagadish@gmail.com>
2024-06-17 16:02:20 +0200

    Refactor model loading to once for all participants, file renaming, and function updates

--- mi/fit_humans.py
+++ mi/fit_humans.py
@@ -7,59 +7,17 @@ from scipy.optimize import differential_evolution, minimize
 import sys
 import re
 from model import TransformerDecoderClassification, TransformerDecoderLinearWeights
+from model_utils import parse_model_path
+from torch.distributions import Bernoulli
 sys.path.insert(0, '/u/ajagadish/ermi/mi')
 SYS_PATH = '/u/ajagadish/ermi'
 
 
-def compute_loglikelihood_human_choices_under_model(env=None, model_path=None, participant=0, beta=1., epsilon=0., method='soft_sigmoid', device='cpu', paired=False, **kwargs):
-
-    # load model
-    # model = torch.load(model_path)[1].to(device) if device == 'cuda' else torch.load(
-    #     model_path, map_location=torch.device('cpu'))[1].to(device)
-    patterns = {
-        "num_hidden": r"num_hidden=(\d+)",
-        "num_layers": r"num_layers=(\d+)",
-        "d_model": r"d_model=(\d+)",
-        "num_head": r"num_head=(\d+)",
-        "noise": r"noise=(\d+\.\d+)",
-        "shuffle": r"shuffle=(True|False)",
-        "paired": r"paired=(True|False)",
-        "loss": r"loss(\w+)"
-    }
-
-    # Initialize a dictionary to store the parsed parameters
-    parameters = {}
-
-    # Parse each parameter from the model_path string
-    for param, pattern in patterns.items():
-        match = re.search(pattern, model_path)
-        if match:
-            parameters[param] = match.group(1)
-
-    num_hidden = int(parameters.get('num_hidden', 0))
-    num_layers = int(parameters.get('num_layers', 0))
-    d_model = int(parameters.get('d_model', 0))
-    num_head = int(parameters.get('num_head', 0))
-    loss_fn = 'nll'  # parameters.get('loss', 'nll')
-    # TODO: does not work other than for binz2022
-    model_max_steps = kwargs.get('model_max_steps', 10)
-    # load model
-    if paired:
-        model = TransformerDecoderLinearWeights(num_input=env.num_dims, num_output=env.num_choices, num_hidden=num_hidden,
-                                                num_layers=num_layers, d_model=d_model, num_head=num_head, max_steps=model_max_steps, loss=loss_fn, device=device).to(device)
-
-    else:
-        model = TransformerDecoderClassification(num_input=env.num_dims, num_output=env.num_choices, num_hidden=num_hidden,
-                                                 num_layers=num_layers, d_model=d_model, num_head=num_head, max_steps=model_max_steps, loss=loss_fn, device=device).to(device)
-    state_dict = torch.load(
-        model_path, map_location=torch.device('cpu'))[1]
-    model.load_state_dict(state_dict)
-    model.to(device)
+def compute_loglikelihood_human_choices_under_model(env, model, participant=0, beta=1., epsilon=0., method='soft_sigmoid', policy='greedy', device='cpu', paired=False, **kwargs):
 
     with torch.no_grad():
 
         # model setup: eval mode and set beta
-        model.eval()
         model.beta = beta
         model.device = device
 
@@ -69,19 +27,15 @@ def compute_loglikelihood_human_choices_under_model(env=None, model_path=None, p
         if not hasattr(env, 'return_prototype'):
             packed_inputs, sequence_lengths, correct_choices, human_choices, _ = outputs
         elif hasattr(env, 'return_prototype') and (env.return_prototype is True):
-            packed_inputs, sequence_lengths, correct_choices, human_choices, stacked_prototypes, _ = outputs
-
-        # set human choices to correct choices
-        # human_choices = correct_choices
-        # randomise human choices
-        # human_choices = torch.randint(0, 2, human_choices.shape).to(device)
+            packed_inputs, sequence_lengths, correct_choices, human_choices, _, _ = outputs
 
         # get model choices
         model_choice_probs = model(
             packed_inputs.float().to(device), sequence_lengths)
+        model_choices = model_choice_probs.round() if policy == 'greedy' else Bernoulli(
+                    probs=model_choice_probs).sample()
 
         if method == 'eps_greedy' or method == 'both':
-
             # make a new tensor containing model_choice_probs for each trial for option 1 and 1-model_choice_probs for option 2
             probs = torch.cat(
                 [1-model_choice_probs, model_choice_probs], axis=2)
@@ -89,29 +43,19 @@ def compute_loglikelihood_human_choices_under_model(env=None, model_path=None, p
             probs = torch.vstack([probs[batch, i, human_choices[batch, i, 0].long(
             )] for batch in range(probs.shape[0]) for i in range(sequence_lengths[batch])])
             probs_with_guessing = probs * (1 - epsilon) + epsilon * 0.5
-            loglikehoods = torch.log(probs_with_guessing)
-            summed_loglikelihoods = loglikehoods.sum()
+            summed_loglikelihoods = torch.log(probs_with_guessing).sum()
 
         elif method == 'soft_sigmoid':
 
             assert epsilon == 0., "epsilon must be 0 for soft_sigmoid"
-            # sum log likelihoods only for unpadded trials per condition and compute chance log likelihood
-            chance_loglikelihood = sum(sequence_lengths) * np.log(0.5)
-
             # compute log likelihoods of human choices under model choice probs (binomial distribution)
-            if model.loss == 'bce':
-                model_choices = model_choice_probs > 0.5 if method == 'greedy' else torch.distributions.Binomial(
-                    probs=model_choice_probs).sample()
-                loglikehoods = torch.distributions.Binomial(
-                    probs=model_choice_probs).log_prob(human_choices)
-
-            elif model.loss == 'nll':
-                model_choices = model_choice_probs.mean > 0.5 if method == 'greedy' else model_choice_probs.sample()
-                loglikehoods = model_choice_probs.log_prob(
-                    human_choices.float())
-
+            loglikehoods = Bernoulli(
+                    probs=model_choice_probs).log_prob(human_choices.float())
             summed_loglikelihoods = torch.vstack(
                 [loglikehoods[idx, :sequence_lengths[idx]].sum() for idx in range(len(loglikehoods))]).sum()
+       
+        # sum log likelihoods only for unpadded trials per condition and compute chance log likelihood
+        chance_loglikelihood = sum(sequence_lengths) * np.log(0.5)
 
         # task performance
         model_choices = torch.concat([model_choices[i, :seq_len] for i, seq_len in enumerate(
@@ -121,89 +65,42 @@ def compute_loglikelihood_human_choices_under_model(env=None, model_path=None, p
         correct_choices = correct_choices.reshape(-1).float().to(device)
         model_accuracy = (model_choices == correct_choices).sum() / \
             correct_choices.numel()
-        # TODO: save human accs per participant
-        # human_accuracy = (human_choices.reshape(-1) ==
-        #                   correct_choices).sum() / correct_choices.numel()
-
+    
     return summed_loglikelihoods, chance_loglikelihood, model_accuracy
-
-
-def grid_search(args):
-
-    if args.method == 'soft_sigmoid':
-        # beta sweep
-        betas = np.arange(0., 10., 0.05)
-        parameters = betas
-
-    elif args.method == 'eps_greedy':
-        # epsilon sweep
-        epsilons = np.arange(0., 1., 0.05)
-        parameters = epsilons
-
-    else:
-        raise NotImplementedError
-
-    def objective(env=None, model_name=None, beta=1., epsilon=0., method='soft_sigmoid', num_runs=1, paradigm='categorisation', **task_features):
-        '''  compute log likelihoods of human choices under model choice probs based on binomial distribution
-        '''
-
-        model_path = f"{SYS_PATH}/{paradigm}/trained_models/{model_name}.pt"
-        participants = env.data.participant.unique()
-        loglikelihoods, p_r2, model_acc = [], [], []
-        for participant in participants:
-            ll, chance_ll, acc = compute_loglikelihood_human_choices_under_model(env=env, model_path=model_path, participant=participant, shuffle_trials=True,
-                                                                                 beta=beta, epsilon=epsilon, method=method, **task_features)
-            loglikelihoods.append(ll)
-            p_r2.append(1 - (ll/chance_ll))
-            model_acc.append(acc)
-
-        loglikelihoods = np.array(loglikelihoods)
-        p_r2 = np.array(p_r2)
-        model_acc = np.array(model_acc)
-
-        return -loglikelihoods, p_r2, model_acc
-
-    nlls, pr2s, accs = [], [], []
-    for idx, param in enumerate(parameters):
-        epsilon = param if args.method == 'eps_greedy' else 0.
-        beta = param if args.method == 'soft_sigmoid' else 1.
-        if args.task_name == 'badham2017':
-            env = Badham2017()
-            # TODO: for task in tasks:
-            task_features = {}
-            nll_per_beta, pr2_per_beta, model_acc_per_beta = objective(
-                env=env, model_name=args.model_name, epsilon=epsilon, beta=beta, method=args.method, num_runs=1, **task_features)
-        elif args.task_name == 'devraj2022':
-            env = Devraj2022()
-            nll_per_beta, pr2_per_beta, model_acc_per_beta = objective(
-                env=env, model_name=args.model_name, epsilon=epsilon, beta=beta, method=args.method, num_runs=1)
-        elif args.task_name == 'binz2022':
-            env = Binz2022()
-            nll_per_beta, pr2_per_beta, model_acc_per_beta = objective(
-                env=env, model_name=args.model_name, epsilon=epsilon, beta=beta, method=args.method, num_runs=1)
-        else:
-            raise NotImplementedError
-        nlls.append(nll_per_beta)
-        pr2s.append(pr2_per_beta)
-        accs.append(model_acc_per_beta)
-
-    return np.array(pr2s), np.array(nlls), accs, parameters
-
 
 def optimize(args):
 
     model_path = f"{SYS_PATH}/{args.paradigm}/trained_models/{args.model_name}.pt"
     if args.task_name == 'badham2017':
         env = Badham2017()
-        task_features = {}
+        task_features = {'model_max_steps': 96}
     elif args.task_name == 'devraj2022':
         env = Devraj2022()
-        task_features = {}
+        task_features = {'model_max_steps': 616}
     elif args.task_name == 'binz2022':
-        env = Binz2022()
-        task_features = {}
+        env = Binz2022(experiment_id=args.exp_id)
+        task_features = {'model_max_steps': 10}
     else:
         raise NotImplementedError
+    
+    # parse model parameters
+    num_hidden, num_layers, d_model, num_head, loss_fn, model_max_steps = parse_model_path(model_path, task_features)
+    
+    # initialise model
+    if args.paired:
+        model = TransformerDecoderLinearWeights(num_input=env.num_dims, num_output=env.num_choices, num_hidden=num_hidden,
+                                                num_layers=num_layers, d_model=d_model, num_head=num_head, max_steps=model_max_steps, loss=loss_fn, device=device).to(device)
+
+    else:
+        model = TransformerDecoderClassification(num_input=env.num_dims, num_output=env.num_choices, num_hidden=num_hidden,
+                                                 num_layers=num_layers, d_model=d_model, num_head=num_head, max_steps=model_max_steps, loss=loss_fn, device=device).to(device)
+    
+    # load model weights
+    state_dict = torch.load(
+        model_path, map_location=device)[1]
+    model.load_state_dict(state_dict)
+    model.to(device)
+    model.eval()
 
     def objective(x, participant):
         epsilon = x[0] if args.method == 'eps_greedy' else 0.
@@ -211,7 +108,7 @@ def optimize(args):
         if args.method == 'both':
             epsilon = x[0]
             beta = x[1]
-        ll, _, _ = compute_loglikelihood_human_choices_under_model(env=env, model_path=model_path, participant=participant, shuffle_trials=True,
+        ll, _, _ = compute_loglikelihood_human_choices_under_model(env=env, model=model, participant=participant, shuffle_trials=True,
                                                                    beta=beta, epsilon=epsilon, method=args.method, paired=args.paired, ** task_features)
         return -ll.numpy()
 
@@ -246,7 +143,7 @@ def optimize(args):
             epsilon = res.x[0]
             beta = res.x[1]
 
-        ll, chance_ll, acc = compute_loglikelihood_human_choices_under_model(env=env, model_path=model_path, participant=participant, shuffle_trials=True,
+        ll, chance_ll, acc = compute_loglikelihood_human_choices_under_model(env=env, model=model, participant=participant, shuffle_trials=True,
                                                                              beta=beta, epsilon=epsilon, method=args.method, paired=args.paired, **task_features)
         nlls.append(-ll)
         pr2s.append(1 - (ll/chance_ll))
@@ -264,12 +161,11 @@ if __name__ == '__main__':
     parser.add_argument('--paradigm', type=str, default='categorisation')
     parser.add_argument('--task-name', type=str,
                         required=True, help='task name')
+    parser.add_argument('--exp-id', type=int, default=1, help='experiment id')
     parser.add_argument('--model-name', type=str,
                         required=True, help='model name')
     parser.add_argument('--method', type=str, default='soft_sigmoid',
                         help='method for computing model choice probabilities')
-    parser.add_argument('--optimizer', action='store_true',
-                        default=False, help='find optimal beta using optimizer')
     parser.add_argument('--num-iters', type=int, default=5)
     parser.add_argument('--paired', action='store_true',
                         default=False, help='paired')
@@ -278,13 +174,11 @@ if __name__ == '__main__':
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    if args.optimizer:
-        pr2s, nlls, accs, parameters = optimize(args)
-        optimizer = 'de'
-    else:
-        pr2s, nlls, accs, parameters = grid_search(args)
-        optimizer = 'grid'
 
+    pr2s, nlls, accs, parameters = optimize(args)
+    optimizer = 'de'
+    
     # save list of results
-    save_path = f"{SYS_PATH}/{args.paradigm}/data/model_comparison/{args.task_name}_{args.model_name[:200]}_paired{args.paired}_{args.method}_{optimizer}.npz"
+    num_hidden, num_layers, d_model, num_head, loss_fn, _, source, condition = parse_model_path(args.model_name, {}, return_data_info=True)
+    save_path = f"{args.paradigm}/data/model_comparison/task={args.task_name}_experiment={args.exp_id}_source={source}_condition={condition}_loss={loss_fn}_paired={args.paired}_method={args.method}_optimizer={optimizer}.npz"
     np.savez(save_path, betas=parameters, nlls=nlls, pr2s=pr2s, accs=accs)
