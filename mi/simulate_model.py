46863f17f79fbf786706c8c5378c29547b79d18a
akjagadish <akshaykjagadish@gmail.com>
2024-08-01 17:21:17 +0200

    simulate ermi model on function learning task

--- mi/simulate_model.py
+++ mi/simulate_model.py
@@ -4,7 +4,7 @@ from envs import Binz2022, Badham2017, Devraj2022, Little2022
 import argparse
 from tqdm import tqdm
 from scipy.optimize import differential_evolution, minimize
-from model import TransformerDecoderClassification, TransformerDecoderLinearWeights
+from model import TransformerDecoderClassification, TransformerDecoderLinearWeights, TransformerDecoderRegression, TransformerDecoderRegressionLinearWeights
 import sys
 import re
 from model_utils import parse_model_path
@@ -12,6 +12,8 @@ from torch.distributions import Bernoulli
 sys.path.insert(0, '/u/ajagadish/ermi/mi')
 SYS_PATH = '/u/ajagadish/ermi'
 
+def compute_mse(x, y, axis=1):
+    return ((x - y) ** 2).mean(axis=axis)
 
 def compute_loglikelihood_human_choices_under_model(env=None, model_path=None, participant=0, beta=1., epsilon=0., policy='greedy', device='cpu', paired=False, **kwargs):
 
@@ -68,6 +70,54 @@ def compute_loglikelihood_human_choices_under_model(env=None, model_path=None, p
 
     return model_accuracy, per_trial_model_accuracy, human_accuracy, per_trial_human_accuracy
 
+def compute_mses_human_predictions_under_model(env=None, model_path=None, participant=0, device='cpu', paired=False, policy='greedy', **kwargs):
+
+    # parse model parameters
+    num_hidden, num_layers, d_model, num_head, loss_fn, model_max_steps = parse_model_path(model_path, kwargs)
+
+    # initialise model
+    if paired:
+        model = TransformerDecoderRegressionLinearWeights(num_input=env.num_dims, num_output=env.num_choices, num_hidden=num_hidden,
+                                                    num_layers=num_layers, d_model=d_model, num_head=num_head, max_steps=model_max_steps, loss=loss_fn, device=device).to(device)
+
+    else:
+        model = TransformerDecoderRegression(num_input=env.num_dims, num_output=env.num_choices, num_hidden=num_hidden,
+                                                 num_layers=num_layers, d_model=d_model, num_head=num_head, max_steps=model_max_steps, loss=loss_fn, device=device).to(device)
+
+    model = torch.load(model_path)[1].to(device) if device=='cuda' else torch.load(model_path, map_location=torch.device('cpu'))[1].to(device)
+        
+    # load model weights
+    # state_dict = torch.load(
+    #     model_path, map_location=torch.device('cpu'))[1]
+    # model.load_state_dict(state_dict)
+    # model.to(device)
+
+    with torch.no_grad():
+
+        # model setup: eval mode and set beta
+        model.eval()
+        model.device = device
+
+        # env setup: sample batch from environment and unpack
+        outputs = env.sample_batch(participant, paired=paired)
+        packed_inputs, sequence_lengths, targets, human_preds, ground_truth_functions = outputs
+        # packed_inputs: (40*24)X25X1
+        
+        # get model preds
+        model_preds = model(
+            packed_inputs.float().to(device), sequence_lengths)
+        model_preds = model_preds.mean if policy == 'greedy' else model_preds.sample()
+
+        # compute metrics
+        model_error = compute_mse(model_preds[:, -1], targets[:, -1])
+        model_preds = torch.concat([model_preds[i, [-1]] for i, _ in enumerate(
+            sequence_lengths)], axis=0).squeeze().float()
+        targets = torch.concat([targets[i, [-1]] for i, _ in enumerate(
+            sequence_lengths)], axis=0).squeeze().float()
+        targets = targets.reshape(-1).float().to(device)
+
+    return model_preds, model_error, targets, human_preds, ground_truth_functions
+
 
 def sample_model(args):
 
@@ -81,24 +131,40 @@ def sample_model(args):
     elif args.task_name == 'binz2022':
         env = Binz2022(experiment_id=args.exp_id)
         task_features = {'model_max_steps': 10}
-    elif args.task_name == 'Little2022':
+    elif args.task_name == 'little2022':
         env = Little2022()
         task_features = {'model_max_steps': 25}
     else:
         raise NotImplementedError
+   
+    participants = env.data.participant.unique()[:5] #TODO
+    if args.task_name in ['little2022']:
 
-    per_trial_accs, per_trial_human_accs, human_accs, accs = [], [], [], []
-    participants = env.data.participant.unique()
-    for participant in participants:
-        beta, epsilon = 1., 0.
-        model_accuracy, per_trial_model_accuracy, human_accuracy, per_trial_human_accuracy = compute_loglikelihood_human_choices_under_model(env=env, model_path=model_path, participant=participant, shuffle_trials=True,
-                                                                                                                   beta=beta, epsilon=epsilon, policy=args.policy, paired=args.paired, **task_features)
-        human_accs.append(human_accuracy)
-        per_trial_accs.append(per_trial_model_accuracy)
-        per_trial_human_accs.append(per_trial_human_accuracy)
-        accs.append(model_accuracy)
+        model_errors, model_preds, targets, human_preds, ground_truth_functions = [], [], [], [], []
+        for participant in participants:
+            model_pred, model_error, target, human_pred, ground_truth_function = compute_mses_human_predictions_under_model(env=env, model_path=model_path, participant=participant, shuffle_trials=True,
+                                                                                                                    paired=args.paired, **task_features)
+            model_preds.append(model_pred)
+            model_errors.append(model_error)
+            targets.append(target)
+            human_preds.append(human_pred)
+            ground_truth_functions.append(ground_truth_function)
 
-    return np.array(accs), torch.stack(per_trial_accs).squeeze().sum(1), np.array(human_accs), np.stack(per_trial_human_accs).squeeze().sum(1)
+        return np.stack(model_preds), np.stack(model_errors), np.stack(targets), np.stack(human_preds), np.stack(ground_truth_functions)
+        
+    else:
+
+        per_trial_accs, per_trial_human_accs, human_accs, accs = [], [], [], []
+        for participant in participants:  
+            beta, epsilon = 1., 0.
+            model_accuracy, per_trial_model_accuracy, human_accuracy, per_trial_human_accuracy = compute_loglikelihood_human_choices_under_model(env=env, model_path=model_path, participant=participant, shuffle_trials=True,
+                                                                                                                    beta=beta, epsilon=epsilon, policy=args.policy, paired=args.paired, **task_features)
+            human_accs.append(human_accuracy)
+            per_trial_accs.append(per_trial_model_accuracy)
+            per_trial_human_accs.append(per_trial_human_accuracy)
+            accs.append(model_accuracy)
+        
+        return np.array(accs), torch.stack(per_trial_accs).squeeze().sum(1), np.array(human_accs), np.stack(per_trial_human_accs).squeeze().sum(1)
 
 
 if __name__ == '__main__':
@@ -116,18 +182,24 @@ if __name__ == '__main__':
                         default=False, help='paired')
     parser.add_argument('--policy', type=str, default='greedy',
                         help='method to use for computing model choices')
-    parser.add_argument('--ess', type=str, default='None',
+    parser.add_argument('--ess', type=str, default='NA',
                     help='constraint')
 
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     args.device = torch.device("cuda" if use_cuda else "cpu")
-
-    model_accuracy, per_trial_model_accuracy, human_accuracy, per_trial_human_accuracy = sample_model(args)
-
-    num_hidden, num_layers, d_model, num_head, loss_fn, _, source, condition = parse_model_path(args.model_name, {}, return_data_info=True)
     
-    # save list of results
-    save_path = f"{args.paradigm}/data/model_simulation/task={args.task_name}_experiment={args.exp_id}_source={source}_condition={condition}_loss={loss_fn}_paired={args.paired}_policy={args.policy}_ess{args.ess}.npz"
-    np.savez(save_path, model_accuracy=model_accuracy,
-             per_trial_model_accuracy=per_trial_model_accuracy, human_accuracy=human_accuracy, per_trial_human_accuracy=per_trial_human_accuracy)
+    num_hidden, num_layers, d_model, num_head, loss_fn, _, source, condition = parse_model_path(args.model_name, {}, return_data_info=True)
+    save_path = f"{args.paradigm}/data/model_simulation/task={args.task_name}_experiment={args.exp_id}_source={source}_condition={condition}_loss={loss_fn}_paired={args.paired}_policy={args.policy}.npz"
+    save_path = save_path if args.ess != 'NA' else save_path.replace('.npz', f"_ess={args.ess}.npz")
+    
+    if args.paradigm == 'functionlearning':
+        model_preds, model_errors, targets, human_preds, ground_truth_functions = sample_model(args)
+        # save list of results
+        np.savez(save_path, model_preds=model_preds, model_errors=model_errors, targets=targets, 
+                 human_preds=human_preds, ground_truth_functions=ground_truth_functions)
+    else:
+        model_accuracy, per_trial_model_accuracy, human_accuracy, per_trial_human_accuracy = sample_model(args)
+        # save list of results
+        np.savez(save_path, model_accuracy=model_accuracy, per_trial_model_accuracy=per_trial_model_accuracy, 
+                 human_accuracy=human_accuracy, per_trial_human_accuracy=per_trial_human_accuracy)
