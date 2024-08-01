6928f817b5388732788ba528b1b94ecd5ae3d357
akjagadish <akshaykjagadish@gmail.com>
2024-08-01 18:01:00 +0200

    prelim version of ERMI linear weights

--- mi/model.py
+++ mi/model.py
@@ -355,3 +355,99 @@ class TransformerDecoder(nn.Module):
     
     def compute_loss(self, model_choices, true_choices):
         return self.criterion(model_choices, true_choices)
+
+class TransformerDecoderRegressionLinearWeights(nn.Module):
+
+    """ Meta-learned inference with vanilla transformer core for regression task with linear mapping from features to predictions"""
+
+    def __init__(self, num_input, num_output, num_hidden, num_layers=1, d_model=256, num_head=1, dropout=0.1, beta=1, max_steps=20, loss='nll', device='cpu') -> None:
+        super(TransformerDecoderRegressionLinearWeights, self).__init__()
+
+        self.device = torch.device(device)
+        self.num_input = num_input + num_output
+        self.num_output = num_output
+        self.num_hidden = num_hidden
+        self.num_layers = num_layers
+        self.num_head = num_head
+
+        # check d_model is correct
+        assert d_model % num_head == 0, "nheads must divide evenly into d_model"
+
+        # embedding layer to move to d_model space
+        self.embedding = nn.Linear(self.num_input, d_model)
+
+        # position encoding
+        self.pos_encoder = PositionalEncoding(
+            d_model=d_model,
+            dropout=dropout,
+            vocab_size=max_steps,
+        )
+
+        # transformer encoder
+        decoder_layer = nn.TransformerDecoderLayer(d_model=d_model, nhead=num_head,
+                                                   dim_feedforward=num_hidden,
+                                                   dropout=dropout, batch_first=True)
+
+        self.transformer = nn.TransformerDecoder(decoder_layer, num_layers)
+
+        # linear layer
+        self.linear_mu = nn.Linear(d_model, num_output)
+        self.linear_logscale = nn.Linear(d_model, num_output)
+
+        # loss
+        assert loss in ['mse', 'nll', 'variational'], "loss must be either mse or nll"
+        self.loss = loss
+
+    def make_sequence_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float(
+            '-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def generate_square_subsequent_mask(self, seq_len, batch_size):
+
+        seq_len = torch.tensor(seq_len)
+        # check if elements of the list seq_len are equal
+        if (seq_len == seq_len[0]).all() and len(seq_len) > 1:
+            mask = self.make_sequence_mask(seq_len[0])
+        else:
+            mask = self.make_sequence_mask(max(seq_len))
+            # repeat the mask matrix self.num_head times
+            mask = mask.unsqueeze(0).repeat(batch_size*self.num_head, 1, 1)
+        return mask
+
+    def forward(self, packed_inputs, sequence_lengths):
+
+        current_inputs = packed_inputs[..., :-self.num_target]
+        packed_inputs = torch.cat(
+            (torch.zeros_like(packed_inputs[:, [0]]), packed_inputs[:, :-1]), dim=1)
+        inputs = self.embedding(packed_inputs.float())
+        inputs_pos_encoded = self.pos_encoder(inputs)
+        tgt_mask = self.generate_square_subsequent_mask(
+            seq_len=sequence_lengths, batch_size=packed_inputs.shape[0])
+        output = self.transformer(inputs_pos_encoded.float().to(self.device), inputs_pos_encoded.float(
+        ).to(self.device), tgt_mask=tgt_mask.to(self.device), memory_mask=tgt_mask.to(self.device))
+
+        w_mu = self.linear_mu(output.to(self.device))
+        mu = (w_mu*current_inputs).sum(-1, keepdim=True).to(self.device)
+        
+        w_std = self.linear_logscale(output.to(self.device))
+        std = torch.exp((w_std*current_inputs).sum(-1, keepdim=True).to(self.device))
+
+        return torch.distributions.Normal(mu, std) if self.loss in ['nll', 'variational'] else mu
+
+    def compute_loss(self, packed_inputs, targets, sequence_lengths=None):
+        if self.loss == 'mse':
+            criterion = nn.MSELoss()
+            model_preds = self.forward(packed_inputs, sequence_lengths)
+            model_preds = torch.concat([model_preds[i, :seq_len] for i, seq_len in enumerate(
+                sequence_lengths)], axis=0).squeeze().float()
+            true_targets = torch.concat(
+                targets, axis=0).float().to(self.device)
+            return criterion(model_preds, true_targets)
+        elif self.loss == 'nll':
+            predictive_posterior = self.forward(
+                packed_inputs, sequence_lengths)
+            return - \
+                predictive_posterior.log_prob(
+                    torch.stack(targets).unsqueeze(2).float().to(self.device)).mean()
