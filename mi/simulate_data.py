78758ded7f059188edda8c0ef53d7ae5620b877e
akjagadish <akshaykjagadish@gmail.com>
2024-10-10 16:11:23 +0200

    simulate data from envs

--- mi/simulate_data.py
+++ mi/simulate_data.py
@@ -1,12 +1,16 @@
-from envs import SyntheticDecisionmakingTask
+from envs import SyntheticDecisionmakingTask, SyntheticFunctionlearningTask
 import argparse
 import torch
 
 
-def simulate_synthetic_decisionmaking_tasks(num_tasks, nonlinear, num_dims, max_steps, noise, device, batch_size=64):
+def simulate_synthetic_tasks(paradigm, num_tasks, nonlinear, num_dims, max_steps, noise, device, batch_size):
 
-    env = SyntheticDecisionmakingTask(num_dims=num_dims, max_steps=max_steps, synthesize_tasks=True,
-                                      batch_size=batch_size, noise=noise, device=device).to(device)
+    if paradigm == 'decisionmaking':
+        env = SyntheticDecisionmakingTask(num_dims=num_dims, max_steps=max_steps, synthesize_tasks=True,
+                                        batch_size=batch_size, noise=noise, device=device).to(device)
+    elif paradigm == 'functionlearning':
+        env = SyntheticFunctionlearningTask(num_dims=num_dims, max_steps=max_steps, batch_size=batch_size, 
+                                            noise=noise, device=device).to(device)
 
     env.save_synthetic_data(num_tasks)
 
@@ -14,6 +18,8 @@ def simulate_synthetic_decisionmaking_tasks(num_tasks, nonlinear, num_dims, max_
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='simulate and save synthetic data')
+    parser.add_argument('--paradigm', type=str, default='decisionmaking',
+                        help='paradigm to simulate data for decisionmaking or functionlearning')
     parser.add_argument('--num-tasks', type=int,
                         default=10000, help='number of tasks')
     parser.add_argument('--num-dims', type=int, default=3,
@@ -23,12 +29,11 @@ if __name__ == "__main__":
     parser.add_argument('--batch-size', type=int,
                         default=100, help='batch size')
     parser.add_argument('--noise', type=float, default=0.0, help='noise level')
-
     parser.add_argument('--nonlinear', action='store_true',
                         default=False, help='simulate nonlinear synthetic data')
 
     args = parser.parse_args()
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-    simulate_synthetic_decisionmaking_tasks(args.num_tasks, args.nonlinear, args.num_dims,
+    simulate_synthetic_tasks(args.paradigm,args.num_tasks, args.nonlinear, args.num_dims,
                                             args.max_steps, args.noise, device, args.batch_size)
